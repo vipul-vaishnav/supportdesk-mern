c30c88e0fba9a0bf0ded6832d5acc3c592dad5cd
Vipul Vaishnav <007vipulrank1@gmail.com>
2022-06-25 13:51:29 +0530

    Prepare for deploy

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,14 @@ import connectDB from './configs/connectDB.js';
 import errorHandler from './middlewares/errorMiddleware.js';
 import userRouter from './routes/userRoutes.js';
 import ticketRouter from './routes/ticketRoutes.js';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+//The 'fileURLToPath' method returns the fully-resolved platform-specific Node.js file path.
+const __filename = fileURLToPath(import.meta.url);
+
+// use the 'dirname()' method from the 'path' module. The 'dirname' method takes a path as a parameter and returns the directory name of the 'path'.
+const __dirname = dirname(__filename);
 
 dotenv.config();
 
@@ -29,6 +37,21 @@ app.use(express.json());
 // Routes
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tickets', ticketRouter);
+
+// Server frontend
+
+if (process.env.NODE_ENV === 'production') {
+  // set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html');
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'welcome to support desk api' });
+  });
+}
 
 // Error handler
 app.use(errorHandler);
